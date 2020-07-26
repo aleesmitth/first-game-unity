05e33c9391dfcd2889d6340ae8d2ec682ed8f282
alan-2083 <67643983+alan-2083@users.noreply.github.com>
2020-07-25 23:00:28 -0300

    23-jul (a la madrugada)

---
+++ Idle v2/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats
+{
+    public NewManagerScript Manager;
+    public float timePlayed;
+    public double totalCurrency1Collected;
+    public double totalCurrency2Collected;
+    public int totalEnemiesDefeated;
+    public int totalUpgradesBought;
+
+    public void UpdateStats(double C1PS, double C2PS)
+    {
+        totalCurrency1Collected += C1PS;
+        totalCurrency2Collected += C2PS;
+    }
+
+    public void UpdateStats(double C1Reward, double C2Reward, int EnemiesDefeated = 1)
+    {
+        totalCurrency1Collected += C1Reward;
+        totalCurrency1Collected += C2Reward;
+        totalEnemiesDefeated += EnemiesDefeated;
+    }
+
+    public void UpdateStats(int UpgradesBought)
+    {
+        totalUpgradesBought += UpgradesBought;
+    }
+
+
+}
---
+++ Idle v2/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade
+{
+        public string UpgradeName = "upgradename";
+        public int UpgradeLevel;
+        public double UpgradePower = 1;
+        public double UpgradeCost = 25;
+        public double UpgradeCostMultiplier = 1.07;
+
+        public Upgrade(string upgradeName)
+        {
+            UpgradeName = upgradeName;
+        }
+
+        public Upgrade(string upgradeName, int upgradeLevel, double upgradePower, double upgradeCost, double upgradeCostMultiplier)
+        {
+            UpgradeName = upgradeName;
+            UpgradeLevel = upgradeLevel;
+            UpgradePower = upgradePower;
+            UpgradeCost = upgradeCost;
+            UpgradeCostMultiplier = upgradeCostMultiplier;
+        }
+
+    public void UpgradeMe(double currencyAmount)
+    {
+        if(currencyAmount >= UpgradeCost)
+        {
+            currencyAmount -= UpgradeCost;
+            UpgradeLevel++;
+            UpgradeCost *= UpgradeCostMultiplier;
+        }
+    }
+}
